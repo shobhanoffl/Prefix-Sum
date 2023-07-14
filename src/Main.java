2a9953dae52055c7b61fa58924afe75f0fed2126
shobhanoffl <shobhankumar.20ads@sonatech.ac.in>
2023-07-15 02:56:13 +0530

    Initial commit done

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+    static int[] calculatePrefixSum(int[] array){
+        int[] prefixSum = new int[array.length];
+        prefixSum[0]=array[0];
+        for(int i=1;i<array.length;i++){
+            prefixSum[i]=prefixSum[i-1]+array[i];
+        }
+        return prefixSum;
+    }
+    public static void main(String[] args) {
+        int[] array = new int[]{1,2,3,4,5};
+        int[] prefixSum = calculatePrefixSum(array);
+
+        for(int i=0; i<array.length; i++){
+            System.out.println(prefixSum[i]);
+        }
+    }
+}
